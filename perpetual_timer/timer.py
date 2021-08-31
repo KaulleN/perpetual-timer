be9f68453c9ac0a030f5bd8f4585d4c683b5e55a
Gleb Nikolaev <lanolesa@ya.ru>
2021-09-01 01:29:51 +0300

    perpetual_timer

---
+++ perpetual_timer/timer.py
@@ -0,0 +1,35 @@
+# -*- encoding: utf-8 -*-
+
+import threading
+from typing import Callable
+from typing import Optional
+
+
+class PerpetualTimer(threading.Thread):
+    def __init__(self, interval: float, function: Callable, initial_interval: Optional[float] = None,
+                 args=None, kwargs=None) -> None:
+        super().__init__()
+        self.interval = interval
+        self.function = function
+        self.initial_interval = initial_interval
+        self.args = args if args is not None else []
+        self.kwargs = kwargs if kwargs is not None else {}
+        self.finished = threading.Event()
+
+    def cancel(self) -> None:
+        """Stop the timer if it hasn't finished yet."""
+        self.finished.set()
+
+    def _run(self, interval: float) -> bool:
+        self.finished.wait(interval)
+        if not self.finished.is_set():
+            self.function(*self.args, **self.kwargs)
+            return True
+        return False
+
+    def run(self) -> None:
+        if self.initial_interval is not None:
+            self._run(self.initial_interval)
+        is_running = True
+        while is_running:
+            is_running = self._run(self.interval)
---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open('README.md') as f:
+    readme = f.read().strip()
+
+setuptools.setup(
+    name='perpetual-timer',
+    version='1.0.0',
+    description='A thread that runs endlessly until stopped with a specified interval between iterations',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/KaulleN/perpetual-timer',
+    author='Gleb Nikolaev',
+    maintainer='Gleb Nikolaev',
+    package_dir='',
+    packages=['perpetual_timer'],
+    python_requires='>=3.6',
+    install_requires=[],
+)
